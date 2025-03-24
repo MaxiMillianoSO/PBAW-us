d691e14d76f55b8b67e286c0228f5100647869a3
Maksym Nidzelskyi <nmmaxi2005@gmail.com>
2025-03-24 18:43:03 +0100

    Update config.php

--- Pr2_kalkulator_kred/Pr2_kalk_okr/config.php
+++ Pr2_kalkulator_kred/Pr2_kalk_okr/config.php
@@ -1,7 +1,7 @@
 <?php
 define('_SERVER_NAME', 'localhost:80');
 define('_SERVER_URL', 'http://'._SERVER_NAME);
-define('_APP_ROOT', '/aplikacje/Pr1_kalk_okr');
+define('_APP_ROOT', '/aplikacje/Pr2_kalk_okr');
 define('_APP_URL', _SERVER_URL._APP_ROOT);
 define("_ROOT_PATH", dirname(__FILE__));
 
